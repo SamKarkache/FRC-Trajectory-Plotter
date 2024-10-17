3fd5fe6630114d9a382de877a4a761b389cebec4
SamKarkache <swkarkache@wpi.edu>
2024-10-16 18:41:16 -0700

    change size of plot to match field size

--- TrajectoryPlotter.py
+++ TrajectoryPlotter.py
@@ -2,6 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import csv
 
+from IPython.core.pylabtools import figsize
+
 
 def plot_trajectory(log_path: str):
     x = []
@@ -19,9 +21,15 @@ def plot_trajectory(log_path: str):
     if len(x) == 0 or len(y) == 0:
         raise Exception(f'No Drive Trajectory data found in {log_path}')
 
-    plt.plot(x, y)
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.plot(x, y)
     plt.title(f'Trajectory Path for {log_path}')
+    plt.xlim(0, 9.144)
+    plt.ylim(0, 9.144 / 2)
     plt.xlabel('X Position')
     plt.ylabel('Y Position')
     plt.grid(True)
     plt.savefig(f'static/images/output.png')
+
+if __name__ == '__main__':
+    plot_trajectory("logfile.csv")
